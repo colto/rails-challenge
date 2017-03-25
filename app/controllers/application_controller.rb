51aa637874ca643b1c7673d8744e4348417474d2
colto <coltomatic@gmail.com>
2017-03-25 12:28:23 -0700

    add bootstrap stuff / basic auth / users index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:home, :about, :contact]
-
-  def home
-  end
-
-  def about
-  end
-
-  def contact
-  end
-
-  def secret
-  end
+  before_action :authenticate_user!
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :installs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+
+  get 'users/show'
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
+
+  root to: 'users#index'
 end
